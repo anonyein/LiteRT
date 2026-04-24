fffb85df36ade0abec1c72fd67d9ed4737d301e5
anonyein <48914448+anonyein@users.noreply.github.com>
2026-04-24 17:02:54 +0800

    Update build_android_package.sh

--- ci/build_android_package.sh
+++ ci/build_android_package.sh
@@ -203,7 +203,7 @@ fi
 
 # TODO(b/503213161): Avoid piggybacking Tensor API's bazel build test on
 # LiteRT's wheel kokoro job.
-bazel build "${BUILD_FLAGS[@]}" //tensor/...
+# bazel build "${BUILD_FLAGS[@]}" //tensor/...
 
 bazel build "${BUILD_FLAGS[@]}" \
     //tflite/java:tensorflow-lite-api \
